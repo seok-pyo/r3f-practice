622854789b13eb3b4e6a3137843a8f4eca789f2c
hongseokpyo <hongseokpyou@gmail.com>
2025-01-11 01:08:37 +0900

    drei practice

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,44 +1,115 @@
-import { useThree, extend, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import { useThree, extend, useFrame } from '@react-three/fiber';
+// import { useRef } from 'react';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import CustomObject from './CustomObject.jsx';
 
-extend({ OrbitControls });
+// extend({ OrbitControls });
+
+// export default function Experience() {
+//   const cubeRef = useRef();
+//   const groupRef = useRef();
+//   const { camera, gl } = useThree();
+//   // delta time
+//   useFrame((state, delta) => {
+//     cubeRef.current.rotation.y += delta;
+
+//     // const angle = state.clock.elapsedTime;
+//     // state.camera.position.x = Math.sin(angle) * 8;
+//     // state.camera.position.z = Math.cos(angle) * 8;
+//     // state.camera.lookAt(0, 0, 0);
+//     // groupRef.current.rotation.y -= delta;
+//   });
+
+//   return (
+//     <>
+//       <orbitControls args={[camera, gl.domElement]} />
+
+//       <directionalLight position={[1, 2, 3]} intensity={2.5} />
+//       <ambientLight intensity={1.5} />
+
+//       <group ref={groupRef}>
+//         <mesh position-x={-2}>
+//           <sphereGeometry />
+//           <meshStandardMaterial color='orange' />
+//         </mesh>
+
+//         <mesh
+//           ref={cubeRef}
+//           rotation-y={Math.PI * 0.23}
+//           position-x={2}
+//           scale={1.5}
+//         >
+//           {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+//           <boxGeometry scale={1.5} />
+//           <meshStandardMaterial color='blue' />
+//         </mesh>
+//       </group>
+
+//       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+//         <planeGeometry />
+//         <meshStandardMaterial color='greenyellow' />
+//       </mesh>
+
+//       <CustomObject />
+//     </>
+//   );
+// }
+import {
+  Html,
+  TransformControls,
+  OrbitControls,
+  PivotControls,
+  Text,
+  Float,
+  MeshReflectorMaterial,
+} from '@react-three/drei';
+import { useRef } from 'react';
 
 export default function Experience() {
   const cubeRef = useRef();
-  const groupRef = useRef();
-  const { camera, gl } = useThree();
-  // delta time
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y -= delta;
-  });
+  const sphereRef = useRef();
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
-      <group ref={groupRef}>
-        <mesh position-x={-2}>
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+      <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={0.5}>
+        <mesh ref={sphereRef} position-x={-2}>
           <sphereGeometry />
-          <meshBasicMaterial color='orange' />
+          <meshStandardMaterial color='orange' />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass='label'
+            center
+            distanceFactor={8}
+            occlude={[sphereRef, cubeRef]}
+          >
+            Hello
+          </Html>
         </mesh>
+      </PivotControls>
 
-        <mesh
-          ref={cubeRef}
-          rotation-y={Math.PI * 0.23}
-          position-x={2}
-          scale={1.5}
-        >
-          {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
-          <boxGeometry scale={1.5} />
-          <meshBasicMaterial color='blue' />
-        </mesh>
-      </group>
+      <mesh ref={cubeRef} scale={1.5} position-x={2}>
+        <boxGeometry scale={1.5} />
+        <meshStandardMaterial color='blue' />
+      </mesh>
+      <TransformControls object={cubeRef} mode='translate' />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color='greenyellow' />
+        {/* <meshStandardMaterial color='greenyellow' /> */}
+        <MeshReflectorMaterial
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.5}
+        />
       </mesh>
+      <Float speed={5} floatIntensity={5}>
+        <Text>H Ell O</Text>
+      </Float>
+      {/* <CustomObject /> */}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,33 @@ import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
 import Experience from './Experience.jsx';
+import {
+  ACESFilmicToneMapping,
+  CineonToneMapping,
+  SRGBColorSpace,
+} from 'three';
 // import { MeshNormalMaterial, TorusKnotGeometry } from 'three';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-  <Canvas>
+  <Canvas
+    // flat
+    // dpr={[1, 2]} // default pixle ratio
+    gl={{
+      antialias: true,
+      toneMapping: ACESFilmicToneMapping,
+      outputColorSpace: SRGBColorSpace,
+    }}
+    // orthographic
+    camera={{
+      fov: 45,
+      // zoom: 50,
+      near: 0.1,
+      far: 200,
+      position: [3, 2, 6],
+    }}
+  >
     <Experience />
   </Canvas>
 );
