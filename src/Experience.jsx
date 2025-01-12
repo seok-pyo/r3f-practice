6fdece2593e010a985b663b16e72f4b357491ab7
hongseokpyo <hongseokpyou@gmail.com>
2025-01-12 21:24:09 +0900

    clean up the file

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -54,61 +54,110 @@
 //     </>
 //   );
 // }
-import {
-  Html,
-  TransformControls,
-  OrbitControls,
-  PivotControls,
-  Text,
-  Float,
-  MeshReflectorMaterial,
-} from '@react-three/drei';
+
+// import {
+//   Html,
+//   TransformControls,
+//   OrbitControls,
+//   PivotControls,
+//   Text,
+//   Float,
+//   MeshReflectorMaterial,
+// } from '@react-three/drei';
+// import { useRef } from 'react';
+
+// export default function Experience() {
+//   const cubeRef = useRef();
+//   const sphereRef = useRef();
+
+//   return (
+//     <>
+//       <OrbitControls makeDefault />
+//       <directionalLight position={[1, 2, 3]} intensity={4.5} />
+//       <ambientLight intensity={1.5} />
+//       <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={0.5}>
+//         <mesh ref={sphereRef} position-x={-2}>
+//           <sphereGeometry />
+//           <meshStandardMaterial color='orange' />
+//           <Html
+//             position={[1, 1, 0]}
+//             wrapperClass='label'
+//             center
+//             distanceFactor={8}
+//             occlude={[sphereRef, cubeRef]}
+//           >
+//             Hello
+//           </Html>
+//         </mesh>
+//       </PivotControls>
+
+//       <mesh ref={cubeRef} scale={1.5} position-x={2}>
+//         <boxGeometry scale={1.5} />
+//         <meshStandardMaterial color='blue' />
+//       </mesh>
+//       <TransformControls object={cubeRef} mode='translate' />
+
+//       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+//         <planeGeometry />
+//         {/* <meshStandardMaterial color='greenyellow' /> */}
+//         <MeshReflectorMaterial
+//           resolution={512}
+//           blur={[1000, 1000]}
+//           mixBlur={1}
+//           mirror={0.5}
+//         />
+//       </mesh>
+//       <Float speed={5} floatIntensity={5}>
+//         <Text>H Ell O</Text>
+//       </Float>
+//       {/* <CustomObject /> */}
+//     </>
+//   );
+// }
+
+import { useThree, extend, useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import CustomObject from './CustomObject.jsx';
+
+extend({ OrbitControls });
 
 export default function Experience() {
   const cubeRef = useRef();
-  const sphereRef = useRef();
+  const groupRef = useRef();
+  const { camera, gl } = useThree();
+  // delta time
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+  });
 
   return (
     <>
-      <OrbitControls makeDefault />
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
-      <ambientLight intensity={1.5} />
-      <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={0.5}>
-        <mesh ref={sphereRef} position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color='orange' />
-          <Html
-            position={[1, 1, 0]}
-            wrapperClass='label'
-            center
-            distanceFactor={8}
-            occlude={[sphereRef, cubeRef]}
-          >
-            Hello
-          </Html>
-        </mesh>
-      </PivotControls>
+      <orbitControls args={[camera, gl.domElement]} />
 
-      <mesh ref={cubeRef} scale={1.5} position-x={2}>
+      <directionalLight position={[1, 2, 3]} intensity={2.5} />
+      <ambientLight intensity={1.5} />
+
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color='orange' />
+      </mesh>
+
+      <mesh
+        ref={cubeRef}
+        rotation-y={Math.PI * 0.23}
+        position-x={2}
+        scale={1.5}
+      >
         <boxGeometry scale={1.5} />
         <meshStandardMaterial color='blue' />
       </mesh>
-      <TransformControls object={cubeRef} mode='translate' />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        {/* <meshStandardMaterial color='greenyellow' /> */}
-        <MeshReflectorMaterial
-          resolution={512}
-          blur={[1000, 1000]}
-          mixBlur={1}
-          mirror={0.5}
-        />
+        <meshStandardMaterial color='greenyellow' />
       </mesh>
-      <Float speed={5} floatIntensity={5}>
-        <Text>H Ell O</Text>
-      </Float>
+
       {/* <CustomObject /> */}
     </>
   );
